d73c2c566e9c38b794bf4132df2340704964580d
Ridzys96 <127649457+Ridzys96@users.noreply.github.com>
2023-04-06 11:00:04 +0530

    Update next_and_spell.py

--- plugins/next_and_spell.py
+++ plugins/next_and_spell.py
@@ -110,7 +110,7 @@ async def advantage_spoll_choker(bot, query):
             k = (movie, files, offset, total_results)
             await auto_filter(bot, query, k)
         else:
-            k = await query.message.edit('This Movie Not Found In DataBase')
+            k = await query.message.edit('This Movie Not Found In DataBase. Please Request your movies from admin @Danyhelp4ktamilbot')
             await asyncio.sleep(10)
             await k.delete()
 
